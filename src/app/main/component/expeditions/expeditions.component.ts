fa8fd9d8f6ef60fb4cd70ec59aa6ef626347f9ee
yurii19 <yurikolechkin@gmail.com>
2023-08-14 09:27:58 +0300

    chore: layouting

--- src/app/main/component/expeditions/expeditions.component.ts
+++ src/app/main/component/expeditions/expeditions.component.ts
@@ -12,5 +12,7 @@ import { TranslateModule } from '@ngx-translate/core';
 export class ExpeditionsComponent {
     gallery: number[] = [...Array(10)]
     imgUrl: string = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS-bargjHS0bbEvbcmCkoUHD649_T1AfqU6tQ&usqp=CAU'
+    categories: string [] = ['Усі','Розвідка','Стаціонарні','Міждисциплінарна',  'Тематична','Відеозапис обряду','Цифровий запис']
+    date = new Date();
 
 }
