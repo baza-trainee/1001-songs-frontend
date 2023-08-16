ea62e734c2a652c44ec33456ea57c92ea754dc45
yurii19 <yurikolechkin@gmail.com>
2023-08-16 13:54:41 +0300

    style: changed main container width calculation on different screens

--- src/app/shared/services/expeditions/expeditions.service.ts
+++ src/app/shared/services/expeditions/expeditions.service.ts
@@ -8,15 +8,15 @@ import { BehaviorSubject } from 'rxjs';
 export class ExpeditionsService {
   
   private  URL: string = 'http://localhost:3000/expeditions';
-  categories: string [] = ['Усі','Розвідка','Стаціонарні','Міждисциплінарна',  'Тематична','Відеозапис обряду','Цифровий запис']
-  expeditons: BehaviorSubject<IExpediton[]> = new BehaviorSubject([{} as IExpediton]);
+  categories: string [] = ['Усі','Розвідка','Стаціонарна','Міждисциплінарна',  'Тематична','Відеозапис обряду','Цифровий запис']
+  $expeditons: BehaviorSubject<IExpediton[]> = new BehaviorSubject([{} as IExpediton]);
 
   constructor( private http: HttpClient) {
       this.uploadExpeditions();
    }
 
   getExpeditions(){
-    return this.expeditons;
+    return this.$expeditons;
   }
 
   getCategories() {
@@ -26,9 +26,9 @@ export class ExpeditionsService {
   private uploadExpeditions(){
     this.http.get(this.URL)
      .subscribe(
-       data => this.expeditons.next(data as IExpediton[]),
+       data => this.$expeditons.next(data as IExpediton[]),
        error => {
-         this.expeditons.next([{} as IExpediton]),
+         this.$expeditons.next([{} as IExpediton]),
          console.error(error.message);
         } 
       )
