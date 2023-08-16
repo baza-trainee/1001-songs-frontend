d014ca0da67f3041a9f385c2102a2e3cd50dea3b
yurii19 <yurikolechkin@gmail.com>
2023-08-16 15:34:50 +0300

    chore: now selected category are highligted

--- src/app/main/component/expeditions/expeditions.component.ts
+++ src/app/main/component/expeditions/expeditions.component.ts
@@ -5,20 +5,23 @@ import { Observable } from 'rxjs';
 import { ExpeditionsService, IExpediton } from 'src/app/shared/services/expeditions/expeditions.service';
 
 @Component({
-    selector: 'app-expeditions',
-    templateUrl: './expeditions.component.html',
-    styleUrls: ['./expeditions.component.scss'],
-    standalone: true,
-    imports: [CommonModule, TranslateModule]
+  selector: 'app-expeditions',
+  templateUrl: './expeditions.component.html',
+  styleUrls: ['./expeditions.component.scss'],
+  standalone: true,
+  imports: [CommonModule, TranslateModule]
 })
 export class ExpeditionsComponent {
+  $expeditions: Observable<IExpediton[]>;
+  categories: string[];
+  selectedCategory: number = 0;
 
-    $expeditions: Observable<IExpediton[]>;
-    categories: string [];
+  constructor(private expeditionsService: ExpeditionsService) {
+    this.categories = this.expeditionsService.getCategories();
+    this.$expeditions = this.expeditionsService.getExpeditions();
+  }
 
-    constructor(private expeditionsService : ExpeditionsService){
-      this.categories = this.expeditionsService.getCategories();
-      this.$expeditions = this.expeditionsService.getExpeditions();
-     }
-
+  selectCategory(id: any) {
+    this.selectedCategory = id;
+  }
 }
