a49c928a00f0227a589cb10a68644531ecdb22da
VolodumurGrushka <gru.v@i.ua>
2023-08-20 12:03:32 +0300

    updated

--- src/app/main/component/news/article/article.component.ts
+++ src/app/main/component/news/article/article.component.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { Article } from '../mockData/mockData';
 import { ArticlesService } from '../services/articles.service';
 import { ActivatedRoute,  } from '@angular/router';
-import { Observable, switchMap } from 'rxjs';
 
 
 @Component({
