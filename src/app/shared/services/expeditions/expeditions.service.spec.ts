ab56389e78781e7690f569ad0fcad34bdd74c194
yurii19 <yurikolechkin@gmail.com>
2023-08-31 20:38:17 +0300

    fix: tests of expeditions service

--- src/app/shared/services/expeditions/expeditions.service.spec.ts
+++ src/app/shared/services/expeditions/expeditions.service.spec.ts
@@ -1,47 +1,33 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ExpeditionsService } from './expeditions.service';
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
-import { HttpClient } from '@angular/common/http';
-import { of } from 'rxjs';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { BehaviorSubject } from 'rxjs';
 import { testCategories, testExpeditionsData } from 'src/mock-data/tests';
 
 describe('ExpeditionsService', () => {
   let service: ExpeditionsService;
-  let httpClient: HttpClient;
-  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule]
     });
     service = TestBed.inject(ExpeditionsService);
-    httpClient = TestBed.inject(HttpClient);
-    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
   it('can test HttpClient.get', () => {
-    const testUrl = `http://localhost:3000/`;
-
-    httpClient.get(`${testUrl}expeditions`).subscribe((data) => {
+    service.getExpeditions().subscribe((data) => {
       expect(data).toEqual(testExpeditionsData);
     });
-    const req = httpTestingController.expectOne({
-      method: 'GET',
-      url: `${testUrl}expeditions`
-    });
-    expect(req.request.method).toEqual('GET');
-    req.flush(testExpeditionsData);
-    httpTestingController.verify();
   });
 
-  it('should return subscribtion for array of categories', () => {
+  it('should return array of categories', () => {
     const categories = service.getCategories();
     expect(categories).toEqual(testCategories);
   });
 
   it('should return subscribtion for array of Iexpeditions', () => {
-    spyOn(service, 'getExpeditions').and.returnValue(of(testExpeditionsData));
+    spyOn(service, 'getExpeditions').and.returnValue(new BehaviorSubject(testExpeditionsData));
     const exps = service.getExpeditions();
     exps.subscribe((data) => {
       expect(data).toEqual(testExpeditionsData);
