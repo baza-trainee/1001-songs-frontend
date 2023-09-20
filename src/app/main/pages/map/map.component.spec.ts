b578dcc32d481a7df6b8093b917caee9d00d2b15
yurii19 <yurikolechkin@gmail.com>
2023-09-20 21:08:27 +0300

    fix: units tests

--- src/app/main/pages/map/map.component.spec.ts
+++ src/app/main/pages/map/map.component.spec.ts
@@ -2,15 +2,21 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MapComponent } from './map.component';
 import { TranslateModule } from '@ngx-translate/core';
+import { NgxsModule, Store } from '@ngxs/store';
+import { MapState } from 'src/app/store/map/map.state';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { GoogleMapsModule } from '@angular/google-maps';
 
 describe('MapComponent', () => {
   let component: MapComponent;
   let fixture: ComponentFixture<MapComponent>;
+  let store: Store;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-    imports: [TranslateModule.forRoot(), MapComponent]
-});
+      imports: [TranslateModule.forRoot(), MapComponent, NgxsModule.forRoot([MapState]), HttpClientModule, GoogleMapsModule]
+    });
+    store = TestBed.inject(Store);
     fixture = TestBed.createComponent(MapComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
--- src/app/shared/services/map/map.service.spec.ts
+++ src/app/shared/services/map/map.service.spec.ts
@@ -1,12 +1,17 @@
 import { TestBed } from '@angular/core/testing';
 
 import { MapService } from './map.service';
+import { NgxsModule } from '@ngxs/store';
+import { MapState } from 'src/app/store/map/map.state';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('MapService', () => {
   let service: MapService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports:[HttpClientModule]
+    });
     service = TestBed.inject(MapService);
   });
 
