d6a1a2be2e8fd027fa5ee82154863a602ad9b102
yurii19 <yurikolechkin@gmail.com>
2023-09-21 12:46:32 +0300

    chore: add map.component tests

--- src/app/main/pages/map/map.component.spec.ts
+++ src/app/main/pages/map/map.component.spec.ts
@@ -6,6 +6,31 @@ import { NgxsModule } from '@ngxs/store';
 import { MapState } from 'src/app/store/map/map.state';
 import { HttpClientModule } from '@angular/common/http';
 import { GoogleMapsModule } from '@angular/google-maps';
+import { cordsMarkers } from 'src/app/mock-data/markers';
+
+export const google = {
+  maps: {
+    Marker: class FakeMarker {
+      private map: unknown;
+      setMap(map: unknown) {
+        this.map = map;
+      }
+    },
+    Size: class {},
+    LatLng: class {},
+    Map: class {
+      setOptions() {}
+      fitBounds() {}
+    }
+  }
+};
+
+export const fakeSelectedMarker = {
+  key: 'marker1',
+  position: { lat: 0, lng: 0 },
+  popup: { title: '', photoUrl: '', countRecords: 0, link: '' }
+};
+class googleMock {}
 
 describe('MapComponent', () => {
   let component: MapComponent;
@@ -15,9 +40,18 @@ describe('MapComponent', () => {
     TestBed.configureTestingModule({
       imports: [TranslateModule.forRoot(), MapComponent, NgxsModule.forRoot([MapState]), HttpClientModule, GoogleMapsModule]
     });
+
+    (window as unknown as { google: googleMock }).google = google;
     fixture = TestBed.createComponent(MapComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+  });
+
+  it('should select markers ', () => {
+    const markers = component.markers$;
+    markers?.subscribe((markers) => {
+      expect(markers[0]).toEqual(cordsMarkers[0]);
+    });
   });
 
   it('should create', () => {
