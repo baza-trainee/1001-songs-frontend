debe46318d478c1be9f68ef0ab371933fc1f7488
yurii19 <yurikolechkin@gmail.com>
2023-09-24 09:38:02 +0300

    chore: added video player component

--- src/app/shared/pipes/sanitizer.pipe.spec.ts
+++ src/app/shared/pipes/sanitizer.pipe.spec.ts
@@ -1,8 +1,16 @@
-import { SanitizerPipe } from './sanitizer.pipe';
+import { TestBed } from '@angular/core/testing';
+import { SanitizePipe } from './sanitizer.pipe';
+import { BrowserModule, DomSanitizer } from '@angular/platform-browser';
 
 describe('SanitizerPipe', () => {
+
+  TestBed.configureTestingModule({
+    imports: [BrowserModule]
+  });
+  let sanitizer: DomSanitizer;
+
   it('create an instance', () => {
-    const pipe = new SanitizerPipe();
+    const pipe = new SanitizePipe(sanitizer);
     expect(pipe).toBeTruthy();
   });
 });
