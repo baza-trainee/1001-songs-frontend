967a08a98cb6eaecdea918678f6efb6e64e8ceb4
shytuleva24 <shutyleva@gmail.com>
2023-09-26 14:05:00 +0300

    fixed line carousel, corrected styles from mobile fast, and put the carousel in shared

--- src/app/main/pages/news/components/articles/articles.component.ts
+++ src/app/main/pages/news/components/articles/articles.component.ts
@@ -1,25 +1,26 @@
 import { Component, DestroyRef, inject, OnInit } from '@angular/core';
 import { NgFor } from '@angular/common';
-import { Router, RouterLink } from '@angular/router';
+import { RouterLink } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 import { ArticlesService } from '../../services/articles.service';
 import { Article } from '../../article.interface';
-import { FilterComponent } from '../filter/filter.component';
+import {ArticleItemComponent} from "./article-item/article-item.component";
+import {FilterComponent} from "../../../../../shared/shared-components/filter/filter.component";
 
 @Component({
   selector: 'app-articles',
   standalone: true,
-  imports: [TranslateModule, NgFor, RouterLink, FilterComponent],
+  imports: [TranslateModule, NgFor, RouterLink, FilterComponent, ArticleItemComponent, FilterComponent],
   templateUrl: './articles.component.html',
   styleUrls: ['./articles.component.scss'],
   providers: [{ provide: ArticlesService, useClass: ArticlesService }]
 })
 export class ArticlesComponent implements OnInit {
+  public readonly categories: string[] = ['Усі', 'Зустрічі', 'Лекції', 'Публікації', 'Майстер-класи', 'Концерти', 'Конференції'];
   public articles!: Article[];
   private readonly articlesService = inject(ArticlesService);
-  private readonly router = inject(Router);
   private readonly destroyRef = inject(DestroyRef);
 
   ngOnInit(): void {
@@ -31,9 +32,5 @@ export class ArticlesComponent implements OnInit {
 
   filteredArticles(articles: Article[]): void {
     this.articles = articles;
-  }
-
-  redirectToArticle(id: number) {
-    this.router.navigate(['/article', id]);
   }
 }
