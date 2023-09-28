b69c802c6db2eb76a3b8c2b421d6799d73392a50
yurii19 <yurikolechkin@gmail.com>
2023-09-28 19:24:10 +0300

    chore: added expansion panel module

--- src/app/main/pages/science/science.component.ts
+++ src/app/main/pages/science/science.component.ts
@@ -3,13 +3,14 @@ import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { Observable, of } from 'rxjs';
 import { CategoryLinkComponent } from './category-link/category-link.component';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 @Component({
   selector: 'app-science',
   templateUrl: './science.component.html',
   styleUrls: ['./science.component.scss'],
   standalone: true,
-  imports: [CommonModule, TranslateModule, CategoryLinkComponent]
+  imports: [CommonModule, TranslateModule, CategoryLinkComponent, MatExpansionModule]
 })
 export class ScienceComponent {
   categories$: Observable<string[]> = of(['Пісні зимового циклу', 'Масляна, Колодій', 'Пісні весняного циклу', 'Звичайні пісні']);
