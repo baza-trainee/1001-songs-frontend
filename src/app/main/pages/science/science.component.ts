be5e5796f78ff5c7a03d6a6ddc3a13d106219a4a
yurii19 <yurikolechkin@gmail.com>
2023-09-28 20:59:15 +0300

    chore: added arrow-down component

--- src/app/main/pages/science/science.component.ts
+++ src/app/main/pages/science/science.component.ts
@@ -4,13 +4,14 @@ import { TranslateModule } from '@ngx-translate/core';
 import { Observable, of } from 'rxjs';
 import { CategoryLinkComponent } from './category-link/category-link.component';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { ArrowDownComponent } from 'src/app/icons/arrow-down/arrow-down.component';
 
 @Component({
   selector: 'app-science',
   templateUrl: './science.component.html',
   styleUrls: ['./science.component.scss'],
   standalone: true,
-  imports: [CommonModule, TranslateModule, CategoryLinkComponent, MatExpansionModule]
+  imports: [CommonModule, TranslateModule, CategoryLinkComponent, MatExpansionModule, ArrowDownComponent]
 })
 export class ScienceComponent {
   categories$: Observable<string[]> = of(['Пісні зимового циклу', 'Масляна, Колодій', 'Пісні весняного циклу', 'Звичайні пісні']);
