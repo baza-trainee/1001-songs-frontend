3fbe2ea49fed1918bed876c6e0661d7a698fe1f5
yurii19 <yurikolechkin@gmail.com>
2023-10-02 16:55:02 +0300

    feat: secected page number higlighted with yellow

--- src/app/main/pages/science/recomendation/recomendation.component.ts
+++ src/app/main/pages/science/recomendation/recomendation.component.ts
@@ -13,15 +13,40 @@ import { recomendations } from '../category-link/recomendations';
   styleUrls: ['./recomendation.component.scss']
 })
 export class RecomendationComponent implements OnInit {
+  private PAGE_SIZE = 5;
   expansionRecomendationArrow = 'bottom';
   recomendations? = recomendations;
+  currentPage: number = 1;
   recomendationPages: number[] = [1];
+
   ngOnInit(): void {
-    this.recomendationPages = Array.from(Array(Math.floor(recomendations!.length / 5) + (recomendations.length % 5)).keys()).map(
-      (el) => el + 1
-    );
+    this.updateRecomendationsList();
+  }
+
+  navigateToPage(specifiedPage: number) {
+    this.currentPage = specifiedPage;
+    this.updateRecomendationsList();
+  }
+
+  navigateToNextPage(pageNumber: number) {
+    const nextPage = this.currentPage + pageNumber;
+    console.log(pageNumber);
+    if (this.recomendationPages.length < nextPage || nextPage < 1) {
+      return;
+    }
+    this.currentPage = nextPage;
+    this.updateRecomendationsList();
   }
   rotateRecomendationArrow() {
     this.expansionRecomendationArrow = this.expansionRecomendationArrow === 'bottom' ? 'top' : 'bottom';
+  }
+
+  updateRecomendationsList() {
+    const fullPages: number = Math.floor(recomendations!.length / this.PAGE_SIZE);
+    const lastPage: number = recomendations.length % this.PAGE_SIZE > 0 ? 1 : 0;
+    this.recomendationPages = Array.from(Array(fullPages + lastPage).keys()).map((el) => el + 1);
+
+    const countFrom = this.PAGE_SIZE * (this.currentPage - 1);
+    this.recomendations = recomendations.slice(countFrom, countFrom + this.PAGE_SIZE);
   }
 }
