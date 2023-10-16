deb1cd24cb83a6cf11ed65280b29d0d37946a32c
yurii19 <yurikolechkin@gmail.com>
2023-10-16 23:43:40 +0300

    fix: linter issues

--- src/app/store/player/player.state.spec.ts
+++ src/app/store/player/player.state.spec.ts
@@ -1,16 +1,16 @@
 import { TestBed } from '@angular/core/testing';
-import { NgxsModule, Store } from '@ngxs/store';
+import { NgxsModule } from '@ngxs/store';
 import { PlayerState } from './player.state';
 import { HttpClientModule } from '@angular/common/http';
 
 describe('PlayerState', () => {
-  let store: Store;
+ // let store: Store;
   let state: PlayerState;
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [NgxsModule.forRoot([PlayerState]), HttpClientModule]
     });
-    store = TestBed.inject(Store);
+   // store = TestBed.inject(Store);
     state = TestBed.inject(PlayerState);
   });
 
