de1cae87ade51e2637d03df8f32ad8ad96d67940
Vitalii Dutov <dutov.vit.vic@gmail.com>
2023-10-23 07:07:23 +0300

    fix: checked in Ukranian version a word project; added two-way functionality for popup closing on nav

--- src/app/shared/shared-components/header/pop-up-menu/pop-up-menu.component.ts
+++ src/app/shared/shared-components/header/pop-up-menu/pop-up-menu.component.ts
@@ -1,37 +1,42 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { TranslateService, TranslateModule } from '@ngx-translate/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { navLinksHeader } from 'src/app/shared/enums/navLinks.enum';
 
 @Component({
-    selector: 'app-pop-up-menu',
-    templateUrl: './pop-up-menu.component.html',
-    styleUrls: ['./pop-up-menu.component.scss'],
-    standalone: true,
-    imports: [RouterLink, RouterLinkActive, TranslateModule, CommonModule]
+  selector: 'app-pop-up-menu',
+  templateUrl: './pop-up-menu.component.html',
+  styleUrls: ['./pop-up-menu.component.scss'],
+  standalone: true,
+  imports: [RouterLink, RouterLinkActive, TranslateModule, CommonModule]
 })
-export class PopUpMenuComponent implements OnInit{
+export class PopUpMenuComponent implements OnInit {
   @Output() changeLangDetection = new EventEmitter<boolean>();
   public lang!: boolean;
   public navLinks = navLinksHeader;
-  constructor(
-    private _translate: TranslateService
-  ){}
+  constructor(private _translate: TranslateService) {}
 
   ngOnInit(): void {
-    this.checkLang()    
+    this.checkLang();
   }
 
   checkLang() {
-    this.lang = this._translate.currentLang === 'en';  
+    this.lang = this._translate.currentLang === 'en';
   }
 
-  selectLang(value: boolean){
+  selectLang(value: boolean) {
     this._translate.use(value ? 'ua' : 'en');
     this.lang = !value;
     this.checkLang();
     this.changeLangDetection.emit(this.lang);
   }
-  
+
+  @Input() isPopupOpen!: boolean;
+  @Output() isPopupOpenChange = new EventEmitter<boolean>();
+
+  togglePopUp() {
+    this.isPopupOpen = !this.isPopupOpen;
+    this.isPopupOpenChange.emit(this.isPopupOpen);
+  }
 }
