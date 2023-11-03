6b86f5f387c8afa9388f74988fd28749b8b4c444
yurii19 <yurikolechkin@gmail.com>
2023-11-03 19:21:27 +0200

    chore: inited component

---
+++ src/app/shared/shared-components/breadcrumbs/breadcrumbs.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-breadcrumbs',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './breadcrumbs.component.html',
+  styleUrls: ['./breadcrumbs.component.scss']
+})
+export class BreadcrumbsComponent {
+
+}
