debdc4f1c9d7eece04576e008aa03d9e8e7d6b50
yurii19 <yurikolechkin@gmail.com>
2023-11-03 20:05:17 +0200

    chore: breadcrumbs compenent added to main component

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -2,15 +2,14 @@ import { Component } from '@angular/core';
 import { FooterComponent } from '../shared/shared-components/footer/footer.component';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '../shared/shared-components/header/header.component';
-import {HomeTapeComponent} from "../shared/shared-components/home-tape/home-tape.component";
+import { HomeTapeComponent } from '../shared/shared-components/home-tape/home-tape.component';
+import { BreadcrumbsComponent } from '../shared/shared-components/breadcrumbs/breadcrumbs.component';
 
 @Component({
-    selector: 'app-main',
-    templateUrl: './main.component.html',
-    styleUrls: ['./main.component.scss'],
-    standalone: true,
-  imports: [HeaderComponent, RouterOutlet, FooterComponent, HomeTapeComponent]
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.scss'],
+  standalone: true,
+  imports: [HeaderComponent, RouterOutlet, FooterComponent, HomeTapeComponent, BreadcrumbsComponent]
 })
-export class MainComponent {
-
-}
+export class MainComponent {}
