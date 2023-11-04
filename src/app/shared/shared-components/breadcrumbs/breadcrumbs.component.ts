1800e7ffb67d45055004079ae0ad4e587a1ea175
yurii19 <yurikolechkin@gmail.com>
2023-11-04 12:36:05 +0200

    chore added redirection

--- src/app/shared/shared-components/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/shared-components/breadcrumbs/breadcrumbs.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavigationEnd, Router } from '@angular/router';
-import { filter } from 'rxjs';
+import { filter, skip } from 'rxjs';
 import * as links from '../../enums/navLinks.enum';
 import { TranslateService, TranslateModule } from '@ngx-translate/core';
 
@@ -13,22 +13,37 @@ import { TranslateService, TranslateModule } from '@ngx-translate/core';
   styleUrls: ['./breadcrumbs.component.scss']
 })
 export class BreadcrumbsComponent implements OnInit {
-  crumbs: any = '';
+  HOME = 'header.nav-menu.home';
+  crumbs: string[] = [];
   constructor(
     private router: Router,
     private _translate: TranslateService
   ) {
-
-    this.router.events.pipe(filter((event) => event instanceof NavigationEnd)).subscribe((route: any) => {
-      const location = route.urlAfterRedirects;
-      console.log(route)
-      const url = links.navLinksHeader.find((link: any) => link.route === location);
-      if (url) {
-        this.crumbs = url.name;
-      }
-    });
+    this.router.events
+      .pipe(
+        // skip(1),
+        filter((event) => event instanceof NavigationEnd)
+      )
+      .subscribe((route: any) => {
+        const path = route.urlAfterRedirects;
+        const pathSegments = path.split('/').filter((el: string) => el !== '');     
+        this.crumbs = [...pathSegments.map((el: string) => this.getTranslateKey(el))];
+      });
   }
   ngOnInit(): void {
-  
+    console.log(this.router.url);
   }
+
+  getTranslateKey(url: string): string {
+    const routeKey = links.navLinksHeader.find((link: any) => link.route === '/' + url);
+    return routeKey ? routeKey.name : url;
+  }
+
+  redirectToPath(event: any){
+    console.log(event)
+  }
+}
+
+export interface crumb {
+  url: string;
 }
