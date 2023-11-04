dc175a9e4a2e36880255a0a3124332267a759b3d
yurii19 <yurikolechkin@gmail.com>
2023-11-04 11:26:06 +0200

    chore: Router injected to breadcrumbs component

--- src/app/shared/shared-components/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/shared-components/breadcrumbs/breadcrumbs.component.ts
@@ -1,13 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
+import * as links from '../../enums/navLinks.enum';
+import { TranslateService, TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-breadcrumbs',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TranslateModule],
   templateUrl: './breadcrumbs.component.html',
   styleUrls: ['./breadcrumbs.component.scss']
 })
-export class BreadcrumbsComponent {
+export class BreadcrumbsComponent implements OnInit {
+  crumbs: any = '';
+  constructor(
+    private router: Router,
+    private _translate: TranslateService
+  ) {
 
+    this.router.events.pipe(filter((event) => event instanceof NavigationEnd)).subscribe((route: any) => {
+      const location = route.urlAfterRedirects;
+      console.log(route)
+      const url = links.navLinksHeader.find((link: any) => link.route === location);
+      if (url) {
+        this.crumbs = url.name;
+      }
+    });
+  }
+  ngOnInit(): void {
+  
+  }
 }
