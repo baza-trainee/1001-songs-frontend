d398ec65e3de7ac408b991888232920cdcb73b50
shutyleva <shytuleva@gmail.com>
2023-11-07 12:12:15 +0200

    fixed router link, added redirect to 404 when no id is available

--- src/app/main/pages/news/components/news-article/news-article.component.ts
+++ src/app/main/pages/news/components/news-article/news-article.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Select } from '@ngxs/store';
-import { Observable } from 'rxjs';
+import { Select, Store } from '@ngxs/store';
+import { filter, first, Observable } from 'rxjs';
+import { TranslateModule } from '@ngx-translate/core';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 
 import { NewsState } from '../../../../../store/news/news.state';
-import { TranslateModule } from '@ngx-translate/core';
-import { RouterLink } from '@angular/router';
 import { Article } from '../../../../../shared/interfaces/article.interface';
+import { FetchArticles, SetSelectedArticle } from "../../../../../store/news/news.actions";
 
 @Component({
   selector: 'app-news-article',
@@ -15,6 +16,32 @@ import { Article } from '../../../../../shared/interfaces/article.interface';
   templateUrl: './news-article.component.html',
   styleUrls: ['./news-article.component.scss']
 })
-export class NewsArticleComponent {
-  @Select(NewsState.getSelectedArticle) selectedArticle$?: Observable<Article>;
+
+export class NewsArticleComponent implements OnInit {
+  @Select(NewsState.getSelectedArticle) selectedArticle$!: Observable<Article>;
+  @Select(NewsState.getArticlesList) articles$!: Observable<Article[]>;
+
+  constructor(
+    private store: Store,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.store.dispatch(new FetchArticles());
+
+    this.articles$
+      .pipe(
+        filter((articles) => articles.length > 0),
+        first()
+      )
+      .subscribe((articles) => {
+        const id: number = this.route.snapshot.params['id']
+        if (articles.some((article: Article) => +article.id === +id)) {
+          this.store.dispatch(new SetSelectedArticle(+id));
+        } else {
+          this.router.navigate(['/404']);
+        }
+    });
+  }
 }
