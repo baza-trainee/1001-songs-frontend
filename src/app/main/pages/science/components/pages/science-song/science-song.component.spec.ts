fda962246d607b41633b8d6d8df75bd384ac6cc3
shutyleva <shytuleva@gmail.com>
2023-11-09 10:46:31 +0200

    fix tests

--- src/app/main/pages/science/components/pages/science-song/science-song.component.spec.ts
+++ src/app/main/pages/science/components/pages/science-song/science-song.component.spec.ts
@@ -3,6 +3,10 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ScienceSongComponent } from './science-song.component';
 import {TranslateModule} from "@ngx-translate/core";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {NgxsModule} from "@ngxs/store";
+import {PlayerState} from "../../../../../../store/player/player.state";
+import {ActivatedRoute, RouterModule} from "@angular/router";
 
 describe('ScienceSongComponent', () => {
   let component: ScienceSongComponent;
@@ -10,7 +14,17 @@ describe('ScienceSongComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [ScienceSongComponent, TranslateModule.forRoot(), BrowserAnimationsModule]
+      imports: [ScienceSongComponent, TranslateModule.forRoot(), BrowserAnimationsModule, RouterModule.forRoot([]), HttpClientTestingModule, NgxsModule.forRoot([PlayerState])],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: {
+              params: { id: 'some_dummy_id' }
+            }
+          }
+        }
+      ]
     });
     fixture = TestBed.createComponent(ScienceSongComponent);
     component = fixture.componentInstance;
