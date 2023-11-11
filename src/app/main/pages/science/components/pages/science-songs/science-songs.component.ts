790d1c671f3120c937303a2d848cdfb86c7f6da0
shutyleva <shytuleva@gmail.com>
2023-11-11 15:21:00 +0200

    add static mockup science categories

--- src/app/main/pages/science/components/pages/science-songs/science-songs.component.ts
+++ src/app/main/pages/science/components/pages/science-songs/science-songs.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {BreadcrumbsComponent} from "../../../../../../shared/shared-components/breadcrumbs/breadcrumbs.component";
-import {Observable} from "rxjs";
+import {Observable, take} from "rxjs";
 import {ActivatedRoute, Router} from "@angular/router";
 import {
   MultichanelPlayerComponent
@@ -15,6 +15,7 @@ import {PlayerState} from "../../../../../../store/player/player.state";
 import {Song} from "../../../../../../shared/interfaces/song.interface";
 import {FetchSongsByLocation} from "../../../../../../store/player/player.actions";
 import {TranslateModule} from "@ngx-translate/core";
+import {scienceCategories} from "../../shared-components/category-link/categoriesList";
 
 @Component({
   selector: 'app-science-songs',
@@ -29,6 +30,7 @@ export class ScienceSongsComponent implements OnInit {
   title!: string;
   about1: string = "<p>Діти (віком від п’яти до десяти/дванадцяти років) колядували/щедрували поодинці або невеличкими групками. Хлопчики і дівчатка могли ходити разом. Найчастіше бігали до родичів, сусідів, хрещених.</p> <p>У текстах дитячих колядок і щедрівок переважають мотиви випрошування дарів та ритуальні погрози господарям.</p> <p>Колядки/щедрівки дитячого репертуару мають вузький діапазон (секунда, терція, кварта). Пісні засновані на безкінечному повторі однієї (рідше двох) поспівок. Текст при цьому змінюється, рефрени дитячим пісням не властиві.</p>";
   about2: string = "<p>Зазвичай діти інтонують приблизно, спів часто переходить у скандування без чіткої висоти. Натомість ритм (три базові ритмічні формули) діти відтворюють чітко (див. на картах)</p>";
+
   constructor(
     private router: Router,
     private route: ActivatedRoute,
@@ -37,10 +39,15 @@ export class ScienceSongsComponent implements OnInit {
 
   ngOnInit(): void {
     this.store.dispatch(new FetchSongsByLocation("Ромейки"));
-    // this.route.params.pipe(take(1)).subscribe(params => {
-    //   this.title = scienceCategories.subcategories[params['id'].split('')[0]].links[params['id'].split('')[1]];
-    //   if (!this.title) this.router.navigate(['/404']);
-    // });
+    this.route.params.pipe(take(1)).subscribe(params => {
+      const category = scienceCategories.find(category => category.routerLink === params['category']);
+      const subCategory = category?.genreGroups
+        .flatMap(group => group.subCategories)
+        .find(sub => sub.query === params['id']);
+
+      subCategory?.title ? this.title = subCategory.title : this.router.navigate(['/404']);
+
+    });
   }
 
 }
--- src/app/main/pages/science/components/shared-components/category-link/categoriesList.ts
+++ src/app/main/pages/science/components/shared-components/category-link/categoriesList.ts
@@ -1,52 +1,3 @@
-// export const scienceCategories = [
-//   {
-//     translateKey: 'science.categories.winters',
-//     url: 'https://drive.google.com/uc?export=view&id=1T3rYJ7HC8fMMhZgXuEl5N90B68tWdOJa',
-//     routerLink: 'winters'
-//   },
-//   {
-//     translateKey: 'science.categories.shrove',
-//     url: 'https://drive.google.com/uc?export=download&id=1JUnuVV3CVE99J6xO9_J8-M9Oc6cP53PU',
-//     routerLink: 'shrove'
-//   },
-//   {
-//     translateKey: 'science.categories.spring',
-//     url: 'https://drive.google.com/uc?export=download&id=1YBIFwScM4Uy7y_cnX9sbIdV5tviopune',
-//     routerLink: 'spring'
-//   },
-//   {
-//     translateKey: 'science.categories.common',
-//     url: 'https://drive.google.com/uc?export=download&id=1bgZpKDm_9hQQC0UZlmgxp4YQDYehwgee',
-//     routerLink: 'common'
-//   },
-//   {
-//     translateKey: 'science.categories.summer',
-//     url: 'https://drive.google.com/uc?export=download&id=11e5XIZg594CljkAdS3nt3ZpS-2o4DAqV',
-//     routerLink: 'summer'
-//   },
-//   {
-//     translateKey: 'science.categories.children',
-//     url: 'https://drive.google.com/uc?export=download&id=1KAaf_N2EIxwRFeK9ejQZjTmn6OJFco0f',
-//     routerLink: 'children'
-//   },
-//   {
-//     translateKey: 'science.categories.wedding',
-//     url: 'https://drive.google.com/uc?export=download&id=1haZ94ZyWoaj860o-LJl-Ekz3KfSCSoN1',
-//     routerLink: 'wedding'
-//   },
-//   {
-//     translateKey: 'science.categories.epos',
-//     url: 'https://drive.google.com/uc?export=download&id=1vW77qHA6dTv57Xr7Uq237G9snXt6_A9q',
-//     routerLink: 'epos'
-//   },
-//   {
-//     translateKey: 'science.categories.instrumental',
-//     url: 'https://drive.google.com/uc?export=download&id=1IQVLLGc0a_LsaBIESyQM1yGPooQUVolx',
-//     routerLink: 'instrumental'
-//   }
-// ];
-
-
 import {ScienceCategory} from "../../../../../../shared/interfaces/science.interface";
 
 export const scienceCategories: ScienceCategory[] = [
@@ -166,7 +117,7 @@ export const scienceCategories: ScienceCategory[] = [
   {
     title: "science.categories.family.title",
     url: "https://drive.google.com/uc?export=download&id=1KAaf_N2EIxwRFeK9ejQZjTmn6OJFco0f",
-    routerLink: "",
+    routerLink: "family",
     genreGroups: [
       {
         title: "science.categories.family.musical-decoration.title",
@@ -195,7 +146,7 @@ export const scienceCategories: ScienceCategory[] = [
   {
     title: "science.categories.ordinary-songs.title",
     url: "https://drive.google.com/uc?export=download&id=1bgZpKDm_9hQQC0UZlmgxp4YQDYehwgee",
-    routerLink: "",
+    routerLink: "ordinary-songs",
     genreGroups: [
       {
         title: "science.categories.ordinary-songs.lyrical.title",
@@ -245,12 +196,12 @@ export const scienceCategories: ScienceCategory[] = [
   {
     title: "science.categories.musical-epos.title",
     url: "https://drive.google.com/uc?export=download&id=1vW77qHA6dTv57Xr7Uq237G9snXt6_A9q",
-    routerLink: "",
+    routerLink: "musical-epos",
     genreGroups: [
       {
         title: "science.categories.musical-epos.byliny.title",
         subCategories: [
-          { title: "science.categories.musical-epos.subcategories.kiev-cycle-byliny", query: "kiev-cycle-byliny" },
+          { title: "science.categories.musical-epos.byliny.subcategories.kiev-cycle-byliny", query: "kiev-cycle-byliny" },
         ]
       },
       {
@@ -270,7 +221,7 @@ export const scienceCategories: ScienceCategory[] = [
   {
     title: "science.categories.instrumental-music.title",
     url: "https://drive.google.com/uc?export=download&id=1IQVLLGc0a_LsaBIESyQM1yGPooQUVolx",
-    routerLink: "",
+    routerLink: "instrumental-music",
     genreGroups: [
       {
         title: "science.categories.instrumental-music.ukrainian-traditional.title",
