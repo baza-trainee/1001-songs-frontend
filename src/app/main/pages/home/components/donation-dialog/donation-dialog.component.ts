e3baa29ea038a640467fbfd2bb82c9d7d92e4516
yurii19 <yurikolechkin@gmail.com>
2023-11-30 13:45:45 +0200

    chore: added donation action button

--- src/app/main/pages/home/components/donation-dialog/donation-dialog.component.ts
+++ src/app/main/pages/home/components/donation-dialog/donation-dialog.component.ts
@@ -11,5 +11,7 @@ import { MatButtonModule } from '@angular/material/button';
   styleUrls: ['./donation-dialog.component.scss']
 })
 export class DonationDialogComponent {
+  actions = { copy: 'Copy IBAN', bmc: 'Buy Me A Coffe', patreon: 'Patreon' };
+  purposeMessage = "Безповоротна фінансова допомога від прізвище, ім'я, по-батькові.";
   constructor(@Inject(MAT_DIALOG_DATA) public data: { iban: string; register: string }) {}
 }
