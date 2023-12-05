063f0035a87dd9ee3b7dece1327cbbcc0f61c79b
yurii19 <yurikolechkin@gmail.com>
2023-12-05 14:00:01 +0200

    chore: copy feature added

--- src/app/shared/shared-components/donation-dialog/donation-dialog.component.ts
+++ src/app/shared/shared-components/donation-dialog/donation-dialog.component.ts
@@ -2,6 +2,7 @@ import { Component, Inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
+import { Clipboard } from '@angular/cdk/clipboard';
 
 @Component({
   selector: 'app-donation-dialog',
@@ -15,5 +16,12 @@ export class DonationDialogComponent {
   purposeMessage = "Безповоротна фінансова допомога від прізвище, ім'я, по-батькові.";
   register = '45284215';
   iban = 'UA353052990000026006035028980';
-  constructor(@Inject(MAT_DIALOG_DATA) public data: {}) {}
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: {},
+    private clipboard: Clipboard
+  ) {}
+
+  copyIBAN() {
+    this.clipboard.copy(this.iban);
+  }
 }
