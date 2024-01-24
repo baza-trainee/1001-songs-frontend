c652e78320b7a8489d75909091ab8a92df73f484
shutyleva <shytuleva@gmail.com>
2024-01-24 19:22:37 +0100

    added data output from the database to the donation window

--- src/app/shared/services/donation/donation.service.ts
+++ src/app/shared/services/donation/donation.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import { map, Observable } from "rxjs";
+import {catchError, map, Observable, of} from "rxjs";
 import {API_URL, StatEndpoints} from "../../config/endpoints/stat-endpoints";
-import {Donation, DonationData, PaymentInfo} from "../../interfaces/donation";
+import {Donation, DonationData, PaymentInfo,} from "../../interfaces/donation";
 import {HttpClient} from "@angular/common/http";
 
 @Injectable({
@@ -11,52 +11,40 @@ import {HttpClient} from "@angular/common/http";
 export class DonationService {
   constructor(private http: HttpClient) {}
 
-  fetchLastDonation(): Observable<Donation> {
-    return this.http.get<DonationData[]>(`${API_URL}${StatEndpoints.donation}`).pipe(
-        map((donations) => {
-          const lastDonation = donations.length > 0 ? donations[donations.length - 1] : donations[0];
-          console.log(lastDonation)
-          return this.mapDonationInfo(lastDonation);
-        })
-    );
-  }
-
-  mapDonationInfo(donationData: DonationData): Donation {
-    if (donationData && donationData.info) {
-      return {
-        id: donationData.id,
-        info: this.parsePaymentInfo(donationData.info),
-        iban: donationData.iban,
-        coffee: donationData.coffee,
-        patreon: donationData.patreon,
-        qr: donationData.qr
-      };
-    }
-
-    return {} as Donation;
-  }
-
-  parsePaymentInfo(input: string): PaymentInfo {
-    const parts = input.split('.');
-
-    const organizationMatch = parts[0].match(/(?<=ГО\s")([^"]*)/);
-    const edrpouMatch = parts[1].match(/(?<=ЄДРПОУ:)([^.]*)/);
-    const ibanMatch = parts[2].match(/(?<=IBAN:)([^.]*)/);
-    const bankMatch = parts[3].match(/(?<=АТ\sКБ\s")[^"]*/);
-    const purposeMatch = parts[4].match(/(?<=Призначення\sплатежу:\s")[^"]*/);
-
-    const organization = organizationMatch ? organizationMatch[0] : '';
-    const edrpou = edrpouMatch ? edrpouMatch[0] : '';
-    const iban = ibanMatch ? ibanMatch[0] : '';
-    const bank = bankMatch ? bankMatch[0] : '';
-    const purpose = purposeMatch ? purposeMatch[0] : '';
+  private parsePaymentInfo(info: string): PaymentInfo {
+    const parts = info.split('.');
 
     return {
-      organization,
-      edrpou,
-      iban,
-      bank,
-      purpose
+      organization: parts[0],
+      edrpou: parts[1],
+      iban: parts[2],
+      bank: parts[3],
+      purpose: parts[4]
     };
+  }
+
+  private convertDonationDataToDonation(donationData: DonationData): Donation {
+    const paymentInfo = this.parsePaymentInfo(donationData.info);
+
+    return {
+      id: donationData.id,
+      info: paymentInfo,
+      iban: donationData.iban,
+      coffee: donationData.coffee,
+      patreon: donationData.patreon,
+      qr: donationData.qr,
+    };
+  }
+
+  fetchLastDonation(): Observable<Donation[]> {
+    return this.http.get<DonationData[]>(`${API_URL}${StatEndpoints.donation}`).pipe(
+      catchError((error) => {
+        console.error(error);
+        return of([]);
+      }),
+      map((donationDataArray) =>
+        donationDataArray.map((donationData) => this.convertDonationDataToDonation(donationData))
+      )
+    );
   }
 }
--- src/app/shared/shared-components/donation-dialog/donation-dialog.component.ts
+++ src/app/shared/shared-components/donation-dialog/donation-dialog.component.ts
@@ -4,11 +4,8 @@ import { MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
 import { Clipboard } from '@angular/cdk/clipboard';
 import {DonationService} from "../../services/donation/donation.service";
-import {Donation} from "../../interfaces/donation";
-import {
-  logBuilderStatusWarnings
-} from "@angular-devkit/build-angular/src/builders/browser-esbuild/builder-status-warnings";
 import {Observable} from "rxjs";
+import {Donation} from "../../interfaces/donation";
 
 @Component({
   selector: 'app-donation-dialog',
@@ -19,7 +16,7 @@ import {Observable} from "rxjs";
 })
 
 export class DonationDialogComponent {
-  donationInfo$: Observable<Donation> = this.donationService.fetchLastDonation();
+  donation$: Observable<Donation[]> = this.donationService.fetchLastDonation();
 
   actions = { copy: 'Copy IBAN', bmc: 'Buy Me A Coffe', patreon: 'Patreon' };
   purposeMessage = "Безповоротна фінансова допомога від прізвище, ім'я, по-батькові.";
@@ -27,13 +24,11 @@ export class DonationDialogComponent {
   iban = 'UA353052990000026006035028980';
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: unknown,
-    private clipboard: Clipboard,
+    public clipboard: Clipboard,
     private donationService: DonationService
   ) {}
 
   copyIBAN() {
     this.clipboard.copy(this.iban);
   }
-
-  protected readonly logBuilderStatusWarnings = logBuilderStatusWarnings;
 }
