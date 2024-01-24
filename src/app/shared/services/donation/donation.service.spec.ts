7e5dd67dafd3412963a2f7a1ba456224ce91b562
shutyleva <shytuleva@gmail.com>
2024-01-24 19:47:13 +0100

    added data output from the database to the footer

--- src/app/shared/services/donation/donation.service.spec.ts
+++ src/app/shared/services/donation/donation.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { DonationService } from './donation.service';
+import {HttpClientModule} from "@angular/common/http";
 
 describe('DonationService', () => {
   let service: DonationService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports:[HttpClientModule]
+    });
     service = TestBed.inject(DonationService);
   });
 
--- src/app/shared/services/footer/footer.service.spec.ts
+++ src/app/shared/services/footer/footer.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { FooterService } from './footer.service';
+import {HttpClientModule} from "@angular/common/http";
 
 describe('FooterService', () => {
   let service: FooterService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports:[HttpClientModule]
+    });
     service = TestBed.inject(FooterService);
   });
 
