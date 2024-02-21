85788f218a27ec7099e1674b2a5a35790784a247
shutyleva <shytuleva@gmail.com>
2024-02-21 20:35:31 +0100

    fix exclude

--- src/app/main/pages/project/project.component.ts
+++ src/app/main/pages/project/project.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {ProjectService} from "../../../shared/services/projects/project.service";
-import {Project, ProjectData} from "../../../shared/interfaces/project.interface";
+import {Project} from "../../../shared/interfaces/project.interface";
 import {Observable, Subscription} from "rxjs";
 import {ActivatedRoute} from "@angular/router";
 import {ShareComponent} from "../../../shared/shared-components/share/share.component";
@@ -17,34 +17,36 @@ import {Slide} from "../../../shared/interfaces/slide.interface";
   templateUrl: './project.component.html',
   styleUrls: ['./project.component.scss']
 })
+
 export class ProjectComponent implements OnInit, OnDestroy {
   public project$!: Observable<Project>;
-  private readonly projects$: Observable<ProjectData[]>;
   public projectsSlides!: Slide[];
   private subscriptions: Subscription[] = [];
 
   constructor(
       private projectService: ProjectService,
       private route: ActivatedRoute
-  ) {
-    this.projects$ = this.projectService.fetchProjects();
-    this.subscribeToRouteParams();
-  }
+  ) {}
 
   ngOnInit(): void {
-    if (this.project$) {
-      this.subscriptions.push(this.projects$.subscribe(projects => {
-        this.projectsSlides = projects.map(project => this.projectService.convertToSlide(project));
-      }));
-    }
+    this.subscribeToRouteParams();
+
   }
 
   private subscribeToRouteParams(): void {
     if (this.route.params) {
       this.subscriptions.push(this.route.params.subscribe(params => {
         this.project$ = this.projectService.fetchProjectById(params['id']);
+        this.fetchSliderItems(params['id']);
       }));
     }
+  }
+
+  private fetchSliderItems(id: string): void {
+    this.subscriptions.push(this.projectService.fetchProjects({project_exclude: id}).subscribe(projects => {
+      this.projectsSlides = projects.map(project => this.projectService.convertToSlide(project));
+    }));
+
   }
 
   ngOnDestroy(): void {
