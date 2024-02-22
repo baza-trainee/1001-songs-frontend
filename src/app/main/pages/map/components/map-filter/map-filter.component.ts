129b1b1b4248a6bf32deac36da2a74a68c11e67c
yurii19 <yurikolechkin@gmail.com>
2024-02-22 11:02:54 +0200

    chore: now song find in the local store

--- src/app/main/pages/map/components/map-filter/map-filter.component.ts
+++ src/app/main/pages/map/components/map-filter/map-filter.component.ts
@@ -7,10 +7,10 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 import { MultiselectComponent } from './multiselect/multiselect.component';
 import { SearchInputComponent } from './search-input/search-input.component';
-import {  OptionsSongFilter, SongFilter } from '../../../../../shared/interfaces/map-marker';
+import { OptionsSongFilter, SongFilter } from '../../../../../shared/interfaces/map-marker';
 import { FilterMapState } from '../../../../../store/filter-map/filter-map.state';
 import { mapFilter } from '../../../../../shared/enums/mapFilter';
-import { FetchSongById, FetchSongs } from 'src/app/store/player/player.actions';
+import { FetchSongById, FetchSongs, FindSongById, SelectSong } from 'src/app/store/player/player.actions';
 import { PlayerState } from 'src/app/store/player/player.state';
 import { Song } from 'src/app/shared/interfaces/song.interface';
 import { InitFilterOptions, SetShownOptions } from '../../../../../store/filter-map/filter-map.actions';
@@ -33,7 +33,7 @@ export class MapFilterComponent implements OnInit, OnDestroy {
   isShowFilter = false;
   private destroy$ = new Subject<void>();
 
-  localSongs: { title: string; id: string }[] = [];
+  localSongs: { title: string; id: number }[] = [];
 
   form = new FormGroup({
     country: new FormControl<string[]>([]),
@@ -44,7 +44,7 @@ export class MapFilterComponent implements OnInit, OnDestroy {
     fund: new FormControl<string[]>([])
   });
 
-  titles: { title: string; id: string }[] = [];
+  titles: { title: string; id: number }[] = [];
   previousValue: SongFilter = { ...(this.form.value as SongFilter) };
 
   constructor(private store: Store) {}
@@ -53,7 +53,8 @@ export class MapFilterComponent implements OnInit, OnDestroy {
     this.store.dispatch(new InitFilterOptions());
 
     this.songs.subscribe((songs) => {
-      this.localSongs = songs.map((song) => ({ title: song.title, id: song.id + '' }));
+      console.log(songs)
+      this.localSongs = songs.map((song) => ({ title: song.title, id: song.id }));
     });
 
     this.form
@@ -74,8 +75,10 @@ export class MapFilterComponent implements OnInit, OnDestroy {
       });
   }
 
-  getSelectedSong(event: { title: string; id: string }) {
-    this.store.dispatch(new FetchSongById(event.id));
+  getSelectedSong(event: { title: string; id: string }) { 
+    this.store.dispatch(new FindSongById(Number.parseInt(event.id)));
+    const filter = new SongFilter([event.id]);
+    this.store.dispatch(new FetchMarkers(filter));
   }
 
   selectBlur() {
