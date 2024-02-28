8cbdbd23ae69ea6593f3ed8f9831e472d89df133
shutyleva <shytuleva@gmail.com>
2024-02-28 16:07:41 +0100

    add data in slider

--- src/app/main/pages/news/components/news-card-slider/news-card-slider.component.spec.ts
+++ src/app/main/pages/news/components/news-card-slider/news-card-slider.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NewsCardSliderComponent } from './news-card-slider.component';
+import {ActivatedRoute} from "@angular/router";
+import {TranslateModule, TranslateService} from "@ngx-translate/core";
 
 describe('NewsCardSliderComponent', () => {
   let component: NewsCardSliderComponent;
@@ -8,7 +10,10 @@ describe('NewsCardSliderComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [NewsCardSliderComponent]
+      imports: [NewsCardSliderComponent, TranslateModule.forRoot()],
+      providers: [TranslateService,
+        { provide: ActivatedRoute, useValue: {} }
+      ]
     });
     fixture = TestBed.createComponent(NewsCardSliderComponent);
     component = fixture.componentInstance;
