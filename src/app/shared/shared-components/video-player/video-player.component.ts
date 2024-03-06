5ba13a0b43cfdc9a110ed25de7385268bcffdb33
yurii19 <yurikolechkin@gmail.com>
2024-03-06 18:11:04 +0200

    chore: disable lint on any type in video player component

--- src/app/shared/shared-components/video-player/video-player.component.ts
+++ src/app/shared/shared-components/video-player/video-player.component.ts
@@ -2,7 +2,9 @@ import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { VideoService } from './video.service';
 import { SanitizePipe } from '../../pipes/sanitizer.pipe';
-declare var YT: any;
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+declare const YT: any;
 
 @Component({
   selector: 'app-video-player',
@@ -16,37 +18,37 @@ export class VideoPlayerComponent implements OnInit {
   @Input() widthIcon: number = 48;
 
   isPreviewDisplayed = true;
-  previewUrl: string= '';
-  
+  previewUrl: string = '';
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   private ytPlayer: any;
+
   @ViewChild('playerContainer', { static: true }) playerContainer!: ElementRef;
   playerId: string = 'ytplayer' + Math.random().toString(36).substring(2, 15); // Generate unique player ID;
 
-  constructor(private videoService: VideoService) {
-
-  }
+  constructor(private videoService: VideoService) {}
   ngOnInit(): void {
     // Initialize YouTube player
     this.ytPlayer = new YT.Player(this.playerContainer.nativeElement, {
       videoId: this.videoService.getIdFromUrl(this.srcUrl),
       events: {
-          'onStateChange': this.onPlayerStateChange.bind(this)
+        onStateChange: this.onPlayerStateChange.bind(this)
       }
-  });
+    });
     this.previewUrl = this.videoService.previewFromUrl(this.srcUrl);
-   // this.embeddedUrl = this.videoService.getEmbeddedUrl(this.srcUrl);
+    // this.embeddedUrl = this.videoService.getEmbeddedUrl(this.srcUrl);
   }
 
-  onPlayerStateChange(event: any) {
+  onPlayerStateChange(event: { data: string }) {
     if (event.data === YT.PlayerState.PAUSED) {
-        console.log('Video paused');
-        // Do something when the video is paused
+      console.log('Video paused');
+      // Do something when the video is paused
     }
-}
+  }
 
   playVideo() {
     this.isPreviewDisplayed = false;
-    this.ytPlayer.playVideo()
+    this.ytPlayer.playVideo();
     // const url = this.player.nativeElement.src;
     // this.player.nativeElement.src = url + '&autoplay=1';
   }
