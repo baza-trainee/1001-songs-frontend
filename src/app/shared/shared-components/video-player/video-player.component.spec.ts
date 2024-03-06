1c67c02a9151f233a6d653f07bf99fdcd34dc690
yurii19 <yurikolechkin@gmail.com>
2024-03-06 21:29:55 +0200

    fix: tests

--- src/app/shared/shared-components/video-player/video-player.component.spec.ts
+++ src/app/shared/shared-components/video-player/video-player.component.spec.ts
@@ -7,6 +7,11 @@ describe('VideoPlayerComponent', () => {
   let fixture: ComponentFixture<VideoPlayerComponent>;
 
   beforeEach(() => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    (window as any).YT = {
+      Player: jasmine.createSpy('Player'),
+      PlayerState: { PAUSED: 'paused', PLAYING: 'playing' }
+    };
     TestBed.configureTestingModule({
       imports: [VideoPlayerComponent]
     });
@@ -15,25 +20,14 @@ describe('VideoPlayerComponent', () => {
     fixture.detectChanges();
   });
 
-  // it('should set player property', () => {
-  //   const player = component.player;
-  //   expect(player).toBeTruthy();
-  // });
+  it('should set player property', () => {
+    const player = component.playerId;
+    expect(player).toBeTruthy();
+  });
 
-  // it('should change player src attribute after call playVideo() function', () => {
-  //   const urlBefore = component.player.nativeElement.src;
-  //   component.playVideo();
-  //   expect(component.player.nativeElement.src).not.toEqual(urlBefore);
-  // });
-
-  // it('should set isPreviewDisplayed to false after call playVideo() function', () => {
-  //   component.playVideo();
-  //   expect(component.isPreviewDisplayed).toEqual(false);
-  // });
-
-  // it('should isPreviewDisplayed to be true ', () => {
-  //   expect(component.isPreviewDisplayed).toBe(true);
-  // });
+  it('should isPreviewDisplayed to be true ', () => {
+    expect(component.isPreviewDisplayed).toBe(true);
+  });
 
   it('should create', () => {
     expect(component).toBeTruthy();
