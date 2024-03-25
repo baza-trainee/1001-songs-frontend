1c481f81320677728950cc8e97f9eb4560fc81eb
yurii19 <yurikolechkin@gmail.com>
2024-03-25 10:32:02 +0200

    fix: now news content load after redirecting from slider

--- src/app/shared/shared-components/content-text/content-text.component.ts
+++ src/app/shared/shared-components/content-text/content-text.component.ts
@@ -1,9 +1,9 @@
-import {Component, Input, OnInit} from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {FadeInCarouselComponent} from "../fade-in-carousel/fade-in-carousel.component";
-import {SafeHtmlPipe} from "../../pipes/safe-html.pipe";
-import {FormattingTextService} from "../../services/formatting-text/formating-text.service";
-import {Content} from "../../interfaces/about.interface";
+import { FadeInCarouselComponent } from '../fade-in-carousel/fade-in-carousel.component';
+import { SafeHtmlPipe } from '../../pipes/safe-html.pipe';
+import { FormattingTextService } from '../../services/formatting-text/formating-text.service';
+import { Content } from '../../interfaces/about.interface';
 
 @Component({
   selector: 'app-content-text',
@@ -12,13 +12,16 @@ import {Content} from "../../interfaces/about.interface";
   templateUrl: './content-text.component.html',
   styleUrls: ['./content-text.component.scss']
 })
-export class ContentTextComponent implements OnInit {
-  @Input({required: true}) content: string = "";
+export class ContentTextComponent implements OnInit, OnChanges {
+  @Input({ required: true }) content: string = '';
   public renderedText!: Content[];
 
   constructor(private formattingService: FormattingTextService) {}
+  ngOnChanges(): void {
+    this.renderedText = this.formattingService.splitText(this.content);
+  }
 
   ngOnInit() {
-    this.renderedText = this.formattingService.splitText(this.content);
+   // this.renderedText = this.formattingService.splitText(this.content);
   }
 }
